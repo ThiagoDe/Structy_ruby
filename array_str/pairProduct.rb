e4d62d0bbe83d7a112ccc4569907e358db498b8d
ThiagoDe <17tawi@gmail.com>
2022-05-28 13:12:34 -0700

    Pair Product

---
+++ array_str/pairProduct.rb
@@ -0,0 +1,18 @@
+def pairProduct(arr, target)
+    elements = {}
+    i = 0
+
+    while i < arr.length
+        current = arr[i]
+        product = target / (current * 1.0)
+        p product
+        if elements[product.to_i]
+            return [elements[product.to_i], i] 
+        end
+        elements[current] = i 
+        i += 1
+    end
+end
+
+p pairProduct([3, 2, 5, 4, 1], 8); # [1, 3]
+p pairProduct([4, 6, 8, 2], 16); # -> [2, 3]
