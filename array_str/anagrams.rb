91874ab7a58e3b59bdda4045faf9cab00de75120
ThiagoDe <17tawi@gmail.com>
2022-05-27 18:20:50 -0700

    anagrams ruby

---
+++ array_str/anagrams.rb
@@ -0,0 +1,16 @@
+def anagrams(s1, s2) # t O(n+m)
+    hash1 = Hash.new { |h, k| h[k] = 0}
+    hash2 = Hash.new { |h, k| h[k] = 0}
+
+    s1.each_char do |c|
+        hash1[c] += 1;
+    end
+    s2.each_char do |c|
+        hash2[c] += 1;
+    end
+
+    hash1 == hash2
+end
+
+p anagrams('monkeyswrite', 'newyorktimes'); # -> true
+p anagrams('abbc', 'aabc'); 
