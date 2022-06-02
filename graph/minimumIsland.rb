46736f4d2cbb7a6435e9d67e5e85e6c2f09892d4
ThiagoDe <17tawi@gmail.com>
2022-06-02 00:19:12 -0700

    Minimum Island

--- graph/minimumIsland.rb
+++ graph/minimumIsland.rb
@@ -1,22 +1,22 @@
 require 'set'
 
 grid = [
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'W', 'W', 'L', 'W'],
-  ['W', 'W', 'L', 'L', 'W'],
-  ['L', 'W', 'W', 'L', 'L'],
-  ['L', 'L', 'W', 'W', 'W'],
+  ['L', 'L', 'L'],
+  ['L', 'L', 'L'],
+  ['L', 'L', 'L'],
 ];
 
-def minimumIsland(grid)
+# minimumIsland(grid); #// -> 9
+
+def minimumIsland(grid) # ts O(rc)
     visited = Set.new
     min = Float::INFINITY
     (0...grid.length).each do |r|
         (0...grid[0].length).each do |c|
             if grid[r][c] == 'L'
                 island = traverse(grid, r, c, visited)
-                min = island if island < min 
+                # p island
+                min = island if island > 0 && island < min 
             end
         end
     end
@@ -24,7 +24,20 @@ def minimumIsland(grid)
 end
 
 def traverse(grid, r, c, visited)
-    return if 
+    rBounds = r >= 0 && r < grid.length
+    cBounds = c >= 0 && c < grid[0].length 
+    pos = r.to_s + ',' + c.to_s 
+    return 0 if !rBounds || !cBounds || grid[r][c] == "W"
+    return 0 if visited.include?(pos)
+    visited.add(pos)
+
+    count = 1
+    count += traverse(grid, r + 1, c, visited)
+    count += traverse(grid, r - 1, c, visited)
+    count += traverse(grid, r, c + 1, visited)
+    count += traverse(grid, r, c - 1, visited)
+
+    return count
 end
 
 p minimumIsland(grid); # // -> 2
