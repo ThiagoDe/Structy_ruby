aab12df3282413ce9693b90ac258d4b4f8eec0e3
ThiagoDe <17tawi@gmail.com>
2022-06-03 16:49:03 -0700

    Closest Carrot

--- graph/closestCarrot.rb
+++ graph/closestCarrot.rb
@@ -9,4 +9,31 @@ grid = [
   ['C', 'O', 'O', 'O', 'O'],
 ];
 
+def closestCarrot(grid, rStart, cStart )
+  pos = rStart.to_s + ',' + cStart.to_s
+  visited = Set.new([pos])
+  queue = [ [rStart, cStart, 0 ]]
+
+  while queue.length > 0
+    r, c, distance = queue.shift
+    return distance if grid[r][c] == 'C'
+    deltas = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    deltas.each do |delta|
+      rDelta, cDelta = delta
+      newR = r + rDelta
+      newC = c + cDelta
+      pos = [newR.to_s, ",", newC.to_s]
+      rInbounds = newR >= 0 && newC < grid.length 
+      cInbounds = newC >= 0 && newC < grid[0].length 
+      if !visited.include?(pos) && grid[r][c] != 'X' && rInbounds && cInbounds
+        visited.add(pos)
+        queue.push([ newR, newC, distance + 1])   
+      end
+    end
+  end
+
+  return - 1
+end
+
 p closestCarrot(grid, 1, 2);# // -> 4
