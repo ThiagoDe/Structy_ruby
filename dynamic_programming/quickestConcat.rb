96ac338eebe932b77edf4a08e926c457f256e39d
ThiagoDe <17tawi@gmail.com>
2022-06-10 11:44:59 -0700

    Quickest Concat

--- dynamic_programming/quickestConcat.rb
+++ dynamic_programming/quickestConcat.rb
@@ -1,18 +1,20 @@
-def quickestConcat(s, words)
+def quickestConcat(s, words, memo = {})
+    return memo[s] if memo.has_key?(s)
     return 0 if s == ''
     min = Float::INFINITY
 
     words.each do |word|
         if s.start_with?(word)
-            possible = 1 + quickestConcat(s[word.length..-1], words)
+            possible = 1 + quickestConcat(s[word.length..-1], words, memo)
             min = [min, possible].min 
         end
     end
-    return - 1 if min == 0 
-    return min 
+    return - 1 if min == Float::INFINITY
+    memo[s] = min 
+    return memo[s]
 end
 
 p quickestConcat('caution', ['ca', 'ion', 'caut', 'ut'])#; // -> 2
 p quickestConcat('rongbetty', ['wrong', 'bet']);# // -> -1
 p quickestConcat('respondorreact', ['re', 'or', 'spond', 'act', 'respond'])#; // -> 4
-
+p quickestConcat('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu', ['u', 'uu', 'uuu', 'uuuu', 'uuuuu']);# // -> 7
