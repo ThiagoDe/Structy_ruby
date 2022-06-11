004a9fa9eb22632520e6392043d0085e201fb27e
ThiagoDe <17tawi@gmail.com>
2022-06-11 12:13:29 -0700

    Nesting score

--- stacks/nestingScore.rb
+++ stacks/nestingScore.rb
@@ -1,5 +1,18 @@
 def nestingScore(str)
-
+    stack = []
+    count_close = 0 
+    str.each_char do |char|
+        if char == '['
+            stack.push(char)
+        elsif char == ']' 
+        end
+    end
 end
 
-p nestingScore("[[][][]]");# // -> 6
+p nestingScore("[][][]")# // -> 3
+p nestingScore("[[]]")# // -> 2
+p nestingScore("[[][]]")# // -> 4
+p nestingScore("[[][][]]")# // -> 6
+p nestingScore("[[][]][]")# // -> 5
+p nestingScore("[][[][]][[]]")# // -> 7
+p nestingScore("[[[[[[[][]]]]]]][]")# // -> 129
